3e403c550e6e563a321bb0782878dc7b3abe2747
Halil <halileymensahin456@gmail.com>
2025-01-31 12:10:55 +0300

    try code

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: 'https://seraphairstudio.com',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
