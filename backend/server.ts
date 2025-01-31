c4cdbd1c1600c829c3deed4358d3509c159f1a71
Halil <halileymensahin456@gmail.com>
2025-01-31 12:18:38 +0300

    cors settings

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: 'https://www.seraphairstudio.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
